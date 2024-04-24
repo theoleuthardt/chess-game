637f87e2f5e83b53b23dc0f74ebaef0674c8f60e
Hyejin Kim <hjqueeen@gmail.com>
2024-04-24 13:36:07 +0200

    Add getAvailableCells for Knight

--- src/main/java/hwr/oop/chess/application/figures/Knight.java
+++ src/main/java/hwr/oop/chess/application/figures/Knight.java
@@ -1,40 +1,57 @@
 package hwr.oop.chess.application.figures;
 
 import hwr.oop.chess.application.Cell;
+
 import java.util.logging.Logger;
 
 import java.util.ArrayList;
 
 public class Knight implements Figure {
-  Logger logger = Logger.getLogger(getClass().getName());
-  private static final FigureType type = FigureType.KNIGHT;
-  private final FigureColor color;
+    Logger logger = Logger.getLogger(getClass().getName());
+    private static final FigureType type = FigureType.KNIGHT;
+    private final FigureColor color;
 
-  public Knight(FigureColor color) {
-    this.color = color;
-  }
+    public Knight(FigureColor color) {
+        this.color = color;
+    }
 
-  public ArrayList<Cell> getAvailableCells(Cell currentCell) {
-    ArrayList<Cell> list = new ArrayList<>();
+    public ArrayList<Cell> getAvailableCells(Cell currentCell) {
+        ArrayList<Cell> list = new ArrayList<>();
 
-    return list;
-  }
+        list.add(currentCell.topCell().topLeftCell());
+        list.add(currentCell.topCell().topRightCell());
+        list.add(currentCell.bottomCell().bottomLeftCell());
+        list.add(currentCell.bottomCell().bottomRightCell());
+        list.add(currentCell.leftCell().topLeftCell());
+        list.add(currentCell.leftCell().bottomLeftCell());
+        list.add(currentCell.rightCell().topRightCell());
+        list.add(currentCell.rightCell().bottomRightCell());
 
-  public boolean canMoveTo(Cell prevCell, Cell nextCell) {
-    ArrayList<Cell> availableCell = getAvailableCells(prevCell);
-    logger.info("canMove: " + availableCell.contains(nextCell));
-    return availableCell.contains(nextCell);
-  }
+        // Remove cell if figure is mine
+        for(Cell cell : list){
+            if(cell.getFigure() != null && cell.getFigure().color() == currentCell.getFigure().color()){
+                list.remove(cell);
+            }
+        }
 
-  public char symbol() {
-    return color == FigureColor.WHITE ? 'N' : 'n';
-  }
+        return list;
+    }
 
-  public FigureColor color() {
-    return color;
-  }
+    public boolean canMoveTo(Cell prevCell, Cell nextCell) {
+        ArrayList<Cell> availableCell = getAvailableCells(prevCell);
+        logger.info("canMove: " + availableCell.contains(nextCell));
+        return availableCell.contains(nextCell);
+    }
 
-  public FigureType type() {
-    return type;
-  }
+    public char symbol() {
+        return color == FigureColor.WHITE ? 'N' : 'n';
+    }
+
+    public FigureColor color() {
+        return color;
+    }
+
+    public FigureType type() {
+        return type;
+    }
 }
