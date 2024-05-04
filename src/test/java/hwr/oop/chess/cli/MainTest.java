6861d5a9e55ce44fac4321ff82a46e972feb851c
Hyejin Kim <hjqueeen@gmail.com>
2024-05-04 21:39:34 +0200

    Add testNoArguments, testWithArguments in Maintest

--- src/test/java/hwr/oop/chess/cli/MainTest.java
+++ src/test/java/hwr/oop/chess/cli/MainTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -23,6 +21,38 @@ public class MainTest {
     @AfterEach
     public void restoreStreams() {
         System.setOut(cliAdapter.printStream);
+    }
+
+    @Test
+    public void testNoArguments() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Main.main(new String[]{});
+        String expected = """             
+                \r
+                Chess Commands:\r
+                - cli on <ID> move <FROM> to <TO>:      Move a figure\r                
+                - cli create <ID>:                      Create a new game of chess\r
+                - cli show-board <ID>:                  Show the board\r
+                \r
+                """;
+
+        assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    public void testWithArguments() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Main.main(new String[]{"arg1", "arg2"});
+        String expected = """             
+                \r
+                Arguments: [arg1, arg2]\r
+                """;
+
+        assertEquals(expected, outContent.toString());
     }
 
     @Test
