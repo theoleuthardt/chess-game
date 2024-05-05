c1da01bf39add6a0cb3b2271066057b66c9fa7f8
jonpfote <82118981+jonpfote@users.noreply.github.com>
2024-05-05 21:27:26 +0200

    add functions to get figures by type or string

--- src/main/java/hwr/oop/chess/application/figures/FigureType.java
+++ src/main/java/hwr/oop/chess/application/figures/FigureType.java
@@ -1,10 +1,32 @@
 package hwr.oop.chess.application.figures;
 
+import hwr.oop.chess.cli.InvalidUserInputException;
+
+import java.util.Arrays;
+import java.util.List;
+
 public enum FigureType {
     KING, // König
     QUEEN, // Dame
     ROOK, // Turm
     BISHOP, // Läufer
     KNIGHT, // Pferd
-    PAWN, // Bauer
+    PAWN; // Bauer
+
+
+    public static List<String> allFigureTypes() {
+        return Arrays.stream(FigureType.values()).map(Enum::name).toList();
+    }
+
+    public static FigureType fromString(String figure) {
+        figure = figure.toUpperCase();
+
+        List<String> allTypes = allFigureTypes();
+        for (String figureType : allTypes) {
+            if (figureType.equals(figure)) {
+                return FigureType.valueOf(figureType);
+            }
+        }
+        throw new InvalidUserInputException("The figure type '" + figure + "' is not valid.");
+    }
 }
