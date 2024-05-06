523867ba33166a35f5f592717e5cd396af827f2e
Hyejin Kim <hjqueeen@gmail.com>
2024-05-06 09:55:14 +0200

    Add FenNoationTest

--- src/main/java/hwr/oop/chess/persistence/FenNotation.java
+++ src/main/java/hwr/oop/chess/persistence/FenNotation.java
@@ -4,9 +4,14 @@ package hwr.oop.chess.persistence;
 
 // pawn = "P", knight = "N", bishop = "B", rook = "R", queen = "Q" and king = "K"
 
+import hwr.oop.chess.application.Board;
+import hwr.oop.chess.application.figures.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FenNotation {
-    // TODO connect with board
-    // TODO connect with GameDataManager
     private String cell;
     private String activeColor;
     private String castling;
@@ -32,6 +37,49 @@ public class FenNotation {
     // getter setter
     public String cell() {
         return this.cell;
+    }
+
+    public static void placeFigureFromFEN(Board board, String fenString) {
+        List<String> parts = new ArrayList<>(Arrays.asList(fenString.split(" ")));
+        List<String> rows = new ArrayList<>(Arrays.asList(parts.get(0).split("/")));
+
+        int y = 1;
+        int x = 1;
+        for (String rowString : rows) {
+            while(x < 9){
+                for (char c : rowString.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        int emptySpaces = Character.getNumericValue(c);
+                        x = x + emptySpaces;
+                    } else {
+                        board.findCell(x,y).setFigure(charToFigureType(c));
+                        x++;
+                    }
+                }
+            }
+            y++;
+            x = 1;
+        }
+    }
+
+    public static Figure charToFigureType(char ch) {
+        Figure figure = null;
+        switch (ch) {
+            case 'b' -> figure = new Bishop(FigureColor.BLACK);
+            case 'k' -> figure = new King(FigureColor.BLACK);
+            case 'n' -> figure = new Knight(FigureColor.BLACK);
+            case 'p' -> figure = new Pawn(FigureColor.BLACK);
+            case 'q' -> figure = new Queen(FigureColor.BLACK);
+            case 'r' -> figure = new Rook(FigureColor.BLACK);
+            case 'B' -> figure = new Bishop(FigureColor.WHITE);
+            case 'K' -> figure = new King(FigureColor.WHITE);
+            case 'N' -> figure = new Knight(FigureColor.WHITE);
+            case 'P' -> figure = new Pawn(FigureColor.WHITE);
+            case 'Q' -> figure = new Queen(FigureColor.WHITE);
+            case 'R' -> figure = new Rook(FigureColor.WHITE);
+            default -> figure = null;
+        }
+        return figure;
     }
 
     public void setPosition(String cell) {
