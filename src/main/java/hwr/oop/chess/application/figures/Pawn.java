0a1801dbf7401bdbd0d1acfebf47c1fc77761f53
Aylin Oymak <aylin.oymak@safetyio.com>
2024-05-13 18:49:25 +0200

    Add pawn promotion test

--- src/main/java/hwr/oop/chess/application/figures/Pawn.java
+++ src/main/java/hwr/oop/chess/application/figures/Pawn.java
@@ -8,93 +8,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn implements Figure {
-    // TODO pawn promotion
-    private static final FigureType type = FigureType.PAWN;
-    private final FigureColor color;
+  private static final FigureType type = FigureType.PAWN;
+  private final FigureColor color;
 
-    public Pawn(FigureColor color) {
-        this.color = color;
+  public Pawn(FigureColor color) {
+    this.color = color;
+  }
+
+  private boolean isInStartPosition(Cell cell) {
+    return cell.y() == (color == FigureColor.WHITE ? 2 : 7);
+  }
+
+  private CellDirection forwards() {
+    return color() == FigureColor.WHITE ? CellDirection.TOP : CellDirection.BOTTOM;
+  }
+
+  public List<Cell> getAvailableCells(Cell currentCell) {
+    List<Cell> cells = new ArrayList<>();
+
+    Cell oneFieldForwards = currentCell.cellInDirection(forwards());
+    if (oneFieldForwards == null) {
+      return cells;
     }
 
-    private boolean isInStartPosition(Cell cell) {
-        return cell.y() == (color == FigureColor.WHITE ? 2 : 7);
+    Cell twoFieldForwards = oneFieldForwards.cellInDirection(forwards());
+
+    // move one field forwards
+    if (oneFieldForwards.isFree()) {
+      cells.add(oneFieldForwards);
     }
 
-    private CellDirection forwards() {
-        return color() == FigureColor.WHITE ? CellDirection.TOP : CellDirection.BOTTOM;
+    // move two fields forwards
+    if (isInStartPosition(currentCell)
+        && twoFieldForwards != null
+        && oneFieldForwards.isFree()
+        && twoFieldForwards.isFree()) {
+      cells.add(twoFieldForwards);
     }
 
-    public ArrayList<Cell> getAvailableCells(Cell currentCell) {
-        ArrayList<Cell> cells = new ArrayList<>();
-
-        Cell oneFieldForwards = currentCell.cellInDirection(forwards());
-        if (oneFieldForwards == null) {
-            return cells;
-        }
-
-        Cell twoFieldForwards = oneFieldForwards.cellInDirection(forwards());
-
-        // move one field forwards
-        if (oneFieldForwards.figure() == null) {
-            cells.add(oneFieldForwards);
-        }
-
-        // move two fields forwards
-        if (isInStartPosition(currentCell)
-                && twoFieldForwards != null
-                && oneFieldForwards.figure() == null
-                && twoFieldForwards.figure() == null) {
-            cells.add(twoFieldForwards);
-        }
-
-        // move one field diagonally left
-        Cell diagonalLeftCell = oneFieldForwards.leftCell();
-        if (diagonalLeftCell != null
-                && diagonalLeftCell.figure() != null
-                && diagonalLeftCell.figure().color() != color()) {
-            cells.add(diagonalLeftCell);
-        }
-
-        // move one field diagonally right
-        Cell diagonalRightCell = oneFieldForwards.rightCell();
-        if (diagonalRightCell != null
-                && diagonalRightCell.figure() != null
-                && diagonalRightCell.figure().color() != color()) {
-            cells.add(diagonalRightCell);
-        }
-        return cells;
+    // move one field diagonally left
+    Cell diagonalLeftCell = oneFieldForwards.leftCell();
+    if (diagonalLeftCell != null
+        && diagonalLeftCell.isOccupied()
+        && diagonalLeftCell.figure().color() != color()) {
+      cells.add(diagonalLeftCell);
     }
 
-    public boolean canMoveTo(Cell prevCell, Cell nextCell) {
-        ArrayList<Cell> availableCell = getAvailableCells(prevCell);
-        return availableCell.contains(nextCell);
+    // move one field diagonally right
+    Cell diagonalRightCell = oneFieldForwards.rightCell();
+    if (diagonalRightCell != null
+        && diagonalRightCell.isOccupied()
+        && diagonalRightCell.figure().color() != color()) {
+      cells.add(diagonalRightCell);
     }
+    return cells;
+  }
 
-    private boolean isAbleToPromote(Cell currentCell) {
-        return currentCell.cellInDirection(forwards()) == null;
-    }
+  public boolean canMoveTo(Cell prevCell, Cell nextCell) {
+    List<Cell> availableCell = getAvailableCells(prevCell);
+    return availableCell.contains(nextCell);
+  }
 
-    // Pawn Promotion
-    public void promotePawn(Cell currentCell, FigureType toType) {
-        if (!isAbleToPromote(currentCell)) {
-            throw new InvalidUserInputException("The pawn is not allowed to be promoted as it has not reached the end.");
-        }
-        List<FigureType> canPromoteTo = List.of(FigureType.QUEEN, FigureType.ROOK, FigureType.BISHOP, FigureType.KNIGHT);
-        if (!canPromoteTo.contains(toType)) {
-            throw new InvalidUserInputException("The pawn cannot become a " + toType.name() + ".");
-        }
-        currentCell.setFigure(getFigureFromTypeAndColor(toType, color()));
-    }
+  public boolean isAbleToPromote(Cell currentCell) {
+    return currentCell.cellInDirection(forwards()) == null;
+  }
 
-    public char symbol() {
-        return color == FigureColor.WHITE ? 'P' : 'p';
+  // Pawn Promotion
+  public void promotePawn(Cell currentCell, FigureType toType) {
+    if (!isAbleToPromote(currentCell)) {
+      throw new InvalidUserInputException(
+          "The pawn is not allowed to be promoted as it has not reached the end.");
     }
+    List<FigureType> canPromoteTo =
+        List.of(FigureType.QUEEN, FigureType.ROOK, FigureType.BISHOP, FigureType.KNIGHT);
+    if (!canPromoteTo.contains(toType)) {
+      throw new InvalidUserInputException("The pawn cannot become a " + toType.name() + ".");
+    }
+    currentCell.setFigure(getFigureFromTypeAndColor(toType, color()));
+  }
 
-    public FigureColor color() {
-        return color;
-    }
+  public char symbol() {
+    return color == FigureColor.WHITE ? 'P' : 'p';
+  }
 
-    public FigureType type() {
-        return type;
-    }
+  public FigureColor color() {
+    return color;
+  }
+
+  public FigureType type() {
+    return type;
+  }
 }
--- src/test/java/hwr/oop/chess/application/figures/PawnTest.java
+++ src/test/java/hwr/oop/chess/application/figures/PawnTest.java
@@ -11,134 +11,159 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PawnTest {
-    private Board board;
+  private Board board;
 
-    @BeforeEach
-    public void setUp() {
-        // Initialize the board
-        board = new Board(true);
-    }
+  @BeforeEach
+  public void setUp() {
+    // Initialize the board
+    board = new Board(true);
+  }
 
-    @Test
-    void createPawn() {
-        Pawn pawn = new Pawn(FigureColor.BLACK);
-        assertThat(pawn.color()).isEqualTo(FigureColor.BLACK);
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-    }
+  @Test
+  void createPawn() {
+    Pawn pawn = new Pawn(FigureColor.BLACK);
+    assertThat(pawn.color()).isEqualTo(FigureColor.BLACK);
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+  }
 
-    @ParameterizedTest
-    @ValueSource(ints = {3, 4})
-    void moveWhitePawn_isAllowed(int args) {
-        Cell from = board.findCell('d', 2);
-        Cell to = board.findCell('d', args);
+  @ParameterizedTest
+  @ValueSource(ints = {3, 4})
+  void moveWhitePawn_isAllowed(int args) {
+    Cell from = board.findCell('d', 2);
+    Cell to = board.findCell('d', args);
 
-        Figure pawn = from.figure();
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-        assertThat(pawn.canMoveTo(from, to)).isTrue();
-    }
+    Figure pawn = from.figure();
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+    assertThat(pawn.canMoveTo(from, to)).isTrue();
+  }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 5})
-    void moveWhitePawn_isNotAllowed(int args) {
-        Cell from = board.findCell('d', 2);
-        Cell to = board.findCell('d', args);
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 5})
+  void moveWhitePawn_isNotAllowed(int args) {
+    Cell from = board.findCell('d', 2);
+    Cell to = board.findCell('d', args);
 
-        Figure pawn = from.figure();
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-        assertThat(pawn.canMoveTo(from, to)).isFalse();
-    }
+    Figure pawn = from.figure();
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+    assertThat(pawn.canMoveTo(from, to)).isFalse();
+  }
 
-    @Test
-    void moveWhitePawn_twoFieldsOnlyOnStart() {
-        Cell from = board.findCell('d', 2);
-        Cell to = board.findCell('d', 3);
-        Cell then = board.findCell('d', 5);
+  @Test
+  void moveWhitePawn_twoFieldsOnlyOnStart() {
+    Cell from = board.findCell('d', 2);
+    Cell to = board.findCell('d', 3);
+    Cell then = board.findCell('d', 5);
 
-        Figure pawn = from.figure();
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-        assertThat(pawn.canMoveTo(from, to)).isTrue();
-        assertThat(pawn.canMoveTo(to, then)).isFalse();
-    }
+    Figure pawn = from.figure();
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+    assertThat(pawn.canMoveTo(from, to)).isTrue();
+    assertThat(pawn.canMoveTo(to, then)).isFalse();
+  }
 
-    @ParameterizedTest
-    @ValueSource(ints = {6})
-    void moveBlackPawn_isAllowed(int args) {
-        Cell from = board.findCell('c', 7);
-        Cell to = board.findCell('c', args);
+  @ParameterizedTest
+  @ValueSource(ints = {6})
+  void moveBlackPawn_isAllowed(int args) {
+    Cell from = board.findCell('c', 7);
+    Cell to = board.findCell('c', args);
 
-        Figure pawn = from.figure();
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-        assertThat(pawn.canMoveTo(from, to)).isTrue();
-    }
+    Figure pawn = from.figure();
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+    assertThat(pawn.canMoveTo(from, to)).isTrue();
+  }
 
-    @ParameterizedTest
-    @ValueSource(ints = {8, 7, 4})
-    void moveBlackPawn_isNotAllowed(int args) {
-        Cell from = board.findCell('c', 7);
-        Cell to = board.findCell('c', args);
+  @ParameterizedTest
+  @ValueSource(ints = {8, 7, 4})
+  void moveBlackPawn_isNotAllowed(int args) {
+    Cell from = board.findCell('c', 7);
+    Cell to = board.findCell('c', args);
 
-        Figure pawn = from.figure();
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-        assertThat(pawn.canMoveTo(from, to)).isFalse();
-    }
+    Figure pawn = from.figure();
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+    assertThat(pawn.canMoveTo(from, to)).isFalse();
+  }
 
-    @Test
-    void moveBlackPawn_twoFieldsOnlyOnStart() {
-        Cell from = board.findCell('c', 7);
-        Cell to = board.findCell('c', 6);
-        Cell then = board.findCell('c', 4);
+  @Test
+  void moveBlackPawn_twoFieldsOnlyOnStart() {
+    Cell from = board.findCell('c', 7);
+    Cell to = board.findCell('c', 6);
+    Cell then = board.findCell('c', 4);
 
-        Figure pawn = from.figure();
-        assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
-        assertThat(pawn.canMoveTo(from, to)).isTrue();
-        assertThat(pawn.canMoveTo(to, then)).isFalse();
-    }
+    Figure pawn = from.figure();
+    assertThat(pawn.type()).isEqualTo(FigureType.PAWN);
+    assertThat(pawn.canMoveTo(from, to)).isTrue();
+    assertThat(pawn.canMoveTo(to, then)).isFalse();
+  }
 
-    @Test
-    void movePawn_noCellAvailableOnLastRow() {
-        Board board = new Board(false);
-        Figure blackPawn = new Pawn(FigureColor.BLACK);
-        assertThat(blackPawn.getAvailableCells(board.findCell(3, 1))).isEmpty();
+  @Test
+  void movePawn_noCellAvailableOnLastRow() {
+    Board board = new Board(false);
+    Figure blackPawn = new Pawn(FigureColor.BLACK);
+    assertThat(blackPawn.getAvailableCells(board.findCell(3, 1))).isEmpty();
 
-        Figure whitePawn = new Pawn(FigureColor.WHITE);
-        assertThat(whitePawn.getAvailableCells(board.findCell(3, 8))).isEmpty();
-    }
+    Figure whitePawn = new Pawn(FigureColor.WHITE);
+    assertThat(whitePawn.getAvailableCells(board.findCell(3, 8))).isEmpty();
+  }
 
-    void pawnDiagonalTest(FigureColor pawnColor, FigureColor diagonalColor, boolean expectedResult) {
-        Board board = new Board(false);
+  void pawnDiagonalTest(FigureColor pawnColor, FigureColor diagonalColor, boolean expectedResult) {
+    Board board = new Board(false);
 
-        CellDirection forwards = pawnColor == FigureColor.WHITE ? CellDirection.TOP : CellDirection.BOTTOM;
-        Cell from = board.findCell(4, 4);
-        Cell forwardsLeft = from.cellInDirection(forwards).leftCell();
-        Cell forwardsRight = from.cellInDirection(forwards).rightCell();
+    CellDirection forwards =
+        pawnColor == FigureColor.WHITE ? CellDirection.TOP : CellDirection.BOTTOM;
+    Cell from = board.findCell(4, 4);
+    Cell forwardsLeft = from.cellInDirection(forwards).leftCell();
+    Cell forwardsRight = from.cellInDirection(forwards).rightCell();
 
-        Figure pawn = new Pawn(pawnColor);
-        Figure diagonalRook = diagonalColor == null ? null : new Rook(diagonalColor);
-        Figure diagonalQueen = diagonalColor == null ? null : new Queen(diagonalColor);
+    Figure pawn = new Pawn(pawnColor);
+    Figure diagonalRook = diagonalColor == null ? null : new Rook(diagonalColor);
+    Figure diagonalQueen = diagonalColor == null ? null : new Queen(diagonalColor);
 
-        from.setFigure(pawn);
-        forwardsLeft.setFigure(diagonalRook);
-        forwardsRight.setFigure(diagonalQueen);
+    from.setFigure(pawn);
+    forwardsLeft.setFigure(diagonalRook);
+    forwardsRight.setFigure(diagonalQueen);
 
-        assertThat(pawn.canMoveTo(from, forwardsLeft)).isEqualTo(expectedResult);
-        assertThat(pawn.canMoveTo(from, forwardsRight)).isEqualTo(expectedResult);
-    }
+    assertThat(pawn.canMoveTo(from, forwardsLeft)).isEqualTo(expectedResult);
+    assertThat(pawn.canMoveTo(from, forwardsRight)).isEqualTo(expectedResult);
+  }
 
-    @Test
-    void movePawn_diagonalWithOpponentThereIsAllowed() {
-        pawnDiagonalTest(FigureColor.WHITE, FigureColor.BLACK, true);
-        pawnDiagonalTest(FigureColor.BLACK, FigureColor.WHITE, true);
-    }
+  @Test
+  void movePawn_diagonalWithOpponentThereIsAllowed() {
+    pawnDiagonalTest(FigureColor.WHITE, FigureColor.BLACK, true);
+    pawnDiagonalTest(FigureColor.BLACK, FigureColor.WHITE, true);
+  }
 
-    @Test
-    void movePawn_diagonalWithSameColorThereIsNotAllowed() {
-        pawnDiagonalTest(FigureColor.WHITE, FigureColor.WHITE, false);
-        pawnDiagonalTest(FigureColor.BLACK, FigureColor.BLACK, false);
-    }
+  @Test
+  void movePawn_diagonalWithSameColorThereIsNotAllowed() {
+    pawnDiagonalTest(FigureColor.WHITE, FigureColor.WHITE, false);
+    pawnDiagonalTest(FigureColor.BLACK, FigureColor.BLACK, false);
+  }
 
-    @Test
-    void moveWhitePawn_diagonalIsNotAllowedIfFieldIsEmpty() {
-        pawnDiagonalTest(FigureColor.WHITE, null, false);
-        pawnDiagonalTest(FigureColor.BLACK, null, false);
-    }
+  @Test
+  void moveWhitePawn_diagonalIsNotAllowedIfFieldIsEmpty() {
+    pawnDiagonalTest(FigureColor.WHITE, null, false);
+    pawnDiagonalTest(FigureColor.BLACK, null, false);
+  }
+
+  @Test
+  void isAbleToPromote_WhitePawn() {
+    Pawn pawn = new Pawn(FigureColor.WHITE);
+    Board board = new Board(true);
+    Cell cell = board.findCell('a', 8);
+    cell.setFigure(pawn);
+
+    boolean result = pawn.isAbleToPromote(cell);
+
+    assertThat(result).isTrue();
+  }
+
+  @Test
+  void isAbleToPromote_BlackPawn() {
+    Pawn pawn = new Pawn(FigureColor.BLACK);
+    Board board = new Board(true);
+    Cell cell = board.findCell('a', 1);
+    cell.setFigure(pawn);
+
+    boolean result = pawn.isAbleToPromote(cell);
+
+    assertThat(result).isTrue();
+  }
 }
