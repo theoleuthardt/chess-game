65f2c43cb777750caa941f9b83da597449696b24
gromozdova23 <celina.gromozdova@gmail.com>
2024-05-29 13:56:41 +0200

    Saving Game after Move and Promotion Tests

--- src/test/java/hwr/oop/chess/persistence/CSVFilePersistenceTest.java
+++ src/test/java/hwr/oop/chess/persistence/CSVFilePersistenceTest.java
@@ -37,6 +37,38 @@ class CSVFilePersistenceTest {
   }
 
   @Test
+  void checkIfMoveEventOnBoardSavesChanges() throws IOException {
+    Main.main(new String[] {"create", "9999"});
+    Main.main(new String[] {"on", "9999", "move", "b2", "b3"});
+
+    persistence.setGameId(9999);
+    persistence.loadGame();
+    assertThat(persistence.loadState("fen"))
+            .isEqualTo("rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR b KQkq - 0 0");
+
+    Path csv = Paths.get("game_9999.csv");
+    Files.deleteIfExists(csv);
+  }
+
+  @Test
+  void checkIfPromotionOnBoardSavesChanges() throws IOException {
+    Main.main(new String[] {"create", "1111"});
+
+    persistence.setGameId(1111);
+    persistence.loadGame();
+    persistence.storeState("fen", "2PP4/8/8/8/7k/8/PP6/7K w - - 0 1");
+    persistence.saveGame();
+
+    Main.main(new String[] {"on", "1111", "promote", "c8", "ROOK"});
+
+    persistence.loadGame();
+    assertThat(persistence.loadState("fen")).isEqualTo("2RP4/8/8/8/7k/8/PP6/7K w - - 0 1");
+
+    Path csv = Paths.get("game_1111.csv");
+    Files.deleteIfExists(csv);
+  }
+
+  @Test
   void negativeGameId() {
     assertThatThrownBy(() -> persistence.setGameId(0))
         .isInstanceOf(InvalidUserInputException.class)
