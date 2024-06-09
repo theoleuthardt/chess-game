2d797df55416c6788b81f66bfea2604a8446612d
jonpfote <82118981+jonpfote@users.noreply.github.com>
2024-06-09 11:38:08 +0200

    remove directory of Java binary from git

--- chess.bat
+++ chess.bat
@@ -1,3 +1,2 @@
 @echo off
-@REM java -cp target/chess-game-0.1.0-SNAPSHOT.jar hwr.oop.chess.cli.Main %*
-C:\jdk-22\bin\java -cp target/chess-game-0.1.0-SNAPSHOT.jar hwr.oop.chess.cli.Main %*
+java -cp target/chess-game-0.1.0-SNAPSHOT.jar hwr.oop.chess.cli.Main %*
